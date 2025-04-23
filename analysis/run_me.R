9a7b41c49c5ee208325046c40c7b3d27790a3bb8
C-Monaghan <cormacmonaghan@tutanota.com>
2025-04-23 12:39:20 +0100

    Running simulation :fire:

--- analysis/run_me.R
+++ analysis/run_me.R
@@ -13,7 +13,7 @@ data <- simulate_data(n_subjects = 2500, seed = 123)$data |>
 models <- fit_markov_model(
   data = data, 
   sample_sizes = c(100, 250, 500, 1000), 
-  n_reps = 10, 
+  n_reps = 1000, 
   seed = 125)
 
 # Visualizations ---------------------------------------------------------------
@@ -29,8 +29,8 @@ good_predictions <- estimate_matrices$estimated_transitions_good
 bad_predictions  <- estimate_matrices$estimated_transitions_bad
 
 # Plotting
-plot_transitions(good_predictions, sample_size = "n_1000", obs = FALSE)
-plot_transitions(bad_predictions, sample_size = "n_1000", obs = FALSE)
+plot_transitions(good_predictions, sample_size = "n_1000",rep = 1, obs = FALSE)
+plot_transitions(bad_predictions, sample_size = "n_1000", rep = 1, obs = FALSE)
 
 plot_multiple_transitions(good_predictions, sample_size = "n_100", obs = FALSE)
 plot_multiple_transitions(bad_predictions, sample_size = "n_100", obs = FALSE)
@@ -43,6 +43,59 @@ compare_transition_matrix(
   sample_size = "n_1000",
   rep = 10, 
   type = "bad")
+
+# Calculating distance based metrics
+distances <- calculate_matrix_distances(
+  results = estimate_matrices, 
+  sample_size = c("n_100", "n_250"),
+  rep = 1:10)
+
+distances |>
+  ggplot(aes(x = sample_size, y = value, fill = metric)) +
+  geom_col(colour = "black") +
+  # geom_text(aes(label = mean_distance), vjust = -0.5) +
+  ggokabeito::scale_fill_okabe_ito() +
+  facet_grid(metric ~ model_type) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none"
+  )
+
+
+distances |>
+  group_by(sample_size, model_type, metric) |>
+  dplyr::summarise(mean_distance = mean(value)) |> 
+  dplyr::mutate(mean_distance = round(mean_distance, digits = 2)) |>
+  ggplot(aes(x = metric, y = mean_distance, fill = metric)) +
+  geom_col(colour = "black") +
+  geom_text(aes(label = mean_distance, vjust = -0.5)) +
+  ggokabeito::scale_fill_okabe_ito() +
+  scale_y_continuous(expand = expansion(mult = c(0.075, 0.075))) +
+  labs(
+    title = "Distance based metrics",
+    subtitle = "Across sample sizes",
+    x = "Distance based metric",
+    y = "Mean value") +
+  facet_grid(model_type ~ sample_size) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+    plot.subtitle = element_text(hjust = 0.5, size = 12, face = "bold"),
+    axis.title = element_text(face = "bold", size = 12),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none"
+  )
+
+distances |>
+  dplyr::filter(repitition == 1) |>
+  ggplot(aes(x = metric, y = value, fill = metric)) +
+  geom_col(colour = "black") +
+  facet_grid(model_type ~ sample_size) +
+  ggeasy::easy_remove_legend()
+
+distances
+  
+  
 
 # ALTERNATIVE ------------------------------------------------------------------
 # More complex set up with custom parameters for number of: 
