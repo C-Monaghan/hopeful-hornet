b4b0ffb70b9ca84118f2f5c05bb4616c98692141
C-Monaghan <cormacmonaghan@tutanota.com>
2025-04-30 17:28:02 +0100

    Running simulation :fire:

--- analysis/Scenario_1.R
+++ analysis/Scenario_1.R
@@ -22,7 +22,7 @@ models <- fit_markov_model(
   data = data, 
   sample_sizes = c(100, 250, 500, 1000), 
   n_reps = 1000,
-  parallel = TRUE,
+  parallel = FALSE,
   seed = 125)
 
 # Visualizations ---------------------------------------------------------------
@@ -57,3 +57,31 @@ distances <- calculate_matrix_distances(
   results = estimate_matrices, 
   sample_size = c("n_100", "n_250", "n_500", "n_1000"),
   rep = 1:100)
+
+# Plotting distances -----------------------------------------------------------
+distances |>
+  group_by(sample_size, model_type, metric) |>
+  summarise(mean_distance = mean(value)) |> 
+  mutate(
+    mean_distance = round(mean_distance, digits = 2),
+    sample_size = stringr::str_replace(sample_size, "_", " = "),
+    sample_size = factor(sample_size, levels = c("n = 100", "n = 250", "n = 500", "n = 1000"))) |>
+  ggplot(aes(x = metric, y = mean_distance, fill = metric)) +
+  geom_col(colour = "black") +
+  geom_text(aes(label = mean_distance, vjust = -0.5)) +
+  ggokabeito::scale_fill_okabe_ito() +
+  scale_y_continuous(expand = expansion(mult = c(0.075, 0.075))) +
+  labs(
+    title = "Distance based metrics",
+    subtitle = "Across sample sizes",
+    x = "Distance based metric",
+    y = "Mean value") +
+  facet_grid(model_type ~ sample_size) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+    plot.subtitle = element_text(hjust = 0.5, size = 12, face = "bold"),
+    axis.title = element_text(face = "bold", size = 12),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none"
+  )
--- analysis/Scenario_2.R
+++ analysis/Scenario_2.R
@@ -40,7 +40,69 @@ models <- fit_markov_model(
   data = data, 
   sample_sizes = c(100, 250, 500, 1000), 
   n_reps = 1000, 
-  seed = 125)
+  parallel = TRUE,
+  seed = 126)
+
+# Visualizations ---------------------------------------------------------------
+# Observed transition matrices
+plot_transitions(models$obs_trans, sample_size = "n_1000", rep = 1)
+plot_multiple_transitions(models$obs_trans, sample_size = "n_1000", reps = 1:6)
+
+# Estimated transition matrices
+estimate_matrices <- estimate_transition_matrices(models, models$test_data)
+
+# Splitting based on good and bad
+good_predictions <- estimate_matrices$estimated_transitions_good
+bad_predictions  <- estimate_matrices$estimated_transitions_bad
+
+# Plotting
+plot_transitions(good_predictions, sample_size = "n_1000",rep = 1, obs = FALSE)
+plot_transitions(bad_predictions, sample_size = "n_1000", rep = 1, obs = FALSE)
+
+plot_multiple_transitions(good_predictions, sample_size = "n_100", obs = FALSE)
+plot_multiple_transitions(bad_predictions, sample_size = "n_100", obs = FALSE)
+
+# Compare matrices
+compare_transition_matrix(
+  models = models, 
+  results = good_predictions, 
+  sample_size = "n_1000",
+  rep = 10, 
+  type = "bad")
+
+# Calculating distance based metrics
+distances <- calculate_matrix_distances(
+  results = estimate_matrices, 
+  sample_size = c("n_100", "n_250", "n_500", "n_1000"),
+  rep = 1:100)
+
+# Plotting distances -----------------------------------------------------------
+distances |>
+  group_by(sample_size, model_type, metric) |>
+  summarise(mean_distance = mean(value)) |> 
+  mutate(
+    mean_distance = round(mean_distance, digits = 2),
+    sample_size = stringr::str_replace(sample_size, "_", " = "),
+    sample_size = factor(sample_size, levels = c("n = 100", "n = 250", "n = 500", "n = 1000"))) |>
+  ggplot(aes(x = metric, y = mean_distance, fill = metric)) +
+  geom_col(colour = "black") +
+  geom_text(aes(label = mean_distance, vjust = -0.5)) +
+  ggokabeito::scale_fill_okabe_ito() +
+  scale_y_continuous(expand = expansion(mult = c(0.075, 0.075))) +
+  labs(
+    title = "Distance based metrics",
+    subtitle = "Across sample sizes",
+    x = "Distance based metric",
+    y = "Mean value") +
+  facet_grid(model_type ~ sample_size) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+    plot.subtitle = element_text(hjust = 0.5, size = 12, face = "bold"),
+    axis.title = element_text(face = "bold", size = 12),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none"
+  )
 
 
 
