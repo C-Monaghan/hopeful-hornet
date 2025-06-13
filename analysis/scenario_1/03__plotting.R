f9b281680af0c60bd01d7859c2dda8178ab94f06
C-Monaghan <cormacmonaghan@tutanota.com>
2025-06-13 13:18:48 +0100

    Adding distance plot

--- analysis/scenario_1/03__plotting.R
+++ analysis/scenario_1/03__plotting.R
@@ -29,22 +29,41 @@ distances[, ":=" (obs_mat = NULL, sim_mat = NULL)]
 dist_sum <- distances[, .(value = mean(value)), by = .(parent_block, sub_model, size_label, rep, wave, metric)] |>
   tibble::as_tibble()
 
+# Plotting ---------------------------------------------------------------------
+dis_plot <- dist_sum |>
+  ggplot(aes(x = log(value), y = sub_model, fill = parent_block)) +
+  geom_boxplot(
+    position = ggstance::position_dodgev(height = 0.95, preserve = "single"),
+    outlier.size = 1) +
+  ggokabeito::scale_fill_okabe_ito(alpha = 0.85) +
+  labs(
+    title =  "Distance Metrics by Sub‑Model and Parent Block",
+    subtitle = "True Distribution is: Base Models (True Model)",
+    x     =  "Log(distance)",
+    y     =  NULL,
+    fill  =  NULL
+  ) +
+  facet_grid(size_label ~ metric, scales = "free_x") +
+  theme_minimal(base_size = 12) +
+  theme(
+    panel.grid.major.y   = element_blank(),
+    panel.grid.minor     = element_blank(),
+    strip.background     = element_rect(fill = "#F0F0F0", colour = NA),
+    strip.text           = element_text(face = "bold", size = 11),
+    axis.text.y          = element_text(face = "bold"),
+    axis.text.x          = element_text(angle = 45, hjust = 1),
+    plot.title           = element_text(face = "bold", size = 14, hjust = 0.5),
+    plot.subtitle        = element_text(face = "bold", size = 12, hjust = 0.5),
+    legend.position      = "bottom",
+    legend.background    = element_rect(fill = "transparent"),
+    legend.key           = element_blank()
+  )
 
-    
+# Saving -----------------------------------------------------------------------
+cowplot::save_plot(
+  filename = here::here(path_scenario, "results/distances.png"),
+  plot = dis_plot,
+  base_height = 10, 
+  base_width = 25)
 
-dist_sum |>
-  split(~ parent_block) |>
-  purrr::map(function(data) {
-    data |>
-      filter(wave == "Wave 1 to 2") |>
-      ggplot(aes(x = log(value), y = sub_model, fill = sub_model)) +
-      geom_boxplot() +
-      ggokabeito::scale_fill_okabe_ito() +
-      facet_grid(size_label ~ metric, scales = "free_x") +
-      theme_bw() +
-      theme(legend.position = "none")
-  })
-  
-
-dist_sum
   
